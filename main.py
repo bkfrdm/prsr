9020d6da7e952c55e1d44ccf0673ec174206bcde
bkfrdm <coolpythonista@gmail.com>
2019-02-20 12:02:11 +0300

    main runner is useful

--- main.py
+++ main.py
@@ -1,20 +1,65 @@
-import prsr as Parser
+import csv
+import collections
+import threading
+from time import sleep
+
+import requests
+import lxml.html, cssselect
 
 
-
-with Parser('_gh', '_prx.txt') as get, Parser('_ph', '_pd', '_prx.txt') as post:
-    url = 'http://xhaus.com/headers'
-    Get.request(url)
-
-    print(Get.response.text)
+from prsr import HDParser
 
 
-with Parser('_gh', '_prx.txt') as get, Parser('_ph', '_pd', '_prx.txt') as post:
-    get(URL)
+class Engine:
+    """ Persistent requests wrapper.
+    Handles all errors except system ones,
+    until counter reaches MAX_RETRY value.
+
+    All methods:
+    -> requests.Response
+    """
+    MAX_RETRY = 3
+    DELAY = 1.5
+
+    def GET(self, *args, **kwargs):
+        errors = 0
+        while True:
+            try:
+                sleep(self.DELAY)
+                response = requests.post(*args, **kwargs)
+                response.raise_for_status()
+                return response
+            
+            except Exception as e:
+                if errors == self.MAX_RETRY:
+                    raise ConnectionError(e)
+                
+                print(e.__class__.__name__, e)
+                errors += 1
     
-    tgt1 = get.select('body div div div h2 span')
-    self.assertEqual(tgt1, ']')
+    def POST(self, *args, **kwargs):
+        errors = 0
+        while True:
+            try:
+                sleep(self.DELAY)
+                response = requests.post(*args, **kwargs)
+                response.raise_for_status()
+                return response
+            
+            except Exception as e:
+                if errors == self.MAX_RETRY:
+                    raise ConnectionError(e)
+                
+                print(e.__class__.__name__, e)
+                errors += 1
 
-    tgt2 = get.select('#Film')
-    self.assertEqual(tgt2, 'Film')
+class Scrpr(Engine):
+    pass
 
+def csv_out(filename, CSV_HEADERS, DATA, encoding='utf-8-sig'):
+        with open(filename, 'w', encoding=encoding) as OUT:
+            OUT = csv.writer(OUT, delimiter=';', lineterminator='\n', escapechar='\\')
+            OUT.writerow(CSV_HEADERS)
+
+            for row in DATA['rows']:
+                OUT.writerow([row.get(h, '-') for h in CSV_HEADERS])
